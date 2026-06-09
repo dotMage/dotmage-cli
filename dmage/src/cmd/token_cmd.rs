9ccc6fb8997425d6f653aa3b9e0b79b21db595ba
yepIwt <ReetRay@protonmail.com>
2026-06-09 14:47:59 +0300

    fix: token prefers server token over local

--- dmage/src/cmd/token_cmd.rs
+++ dmage/src/cmd/token_cmd.rs
@@ -6,8 +6,17 @@ use dotmage_client::token;
 use super::{CliError, Context};
 
 pub fn run(ctx: &Context) -> Result<(), CliError> {
-    let server_hash = keychain::server_hash(&ctx.config.server_id());
-    let tokens = token::load_tokens(&server_hash)
+    // Try server token first, then local
+    if let Some(ref url) = ctx.config.server_url {
+        let hash = keychain::server_hash(url);
+        if let Ok(Some(t)) = token::load_tokens(&hash) {
+            println!("{}", t.device_token);
+            return Ok(());
+        }
+    }
+
+    let hash = keychain::server_hash(&ctx.config.server_id());
+    let tokens = token::load_tokens(&hash)
         .map_err(|e| CliError::Other(e.to_string()))?
         .ok_or(CliError::NotAuthenticated)?;
 
